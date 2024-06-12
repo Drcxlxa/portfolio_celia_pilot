b004b11b0dc9c09e314f032fc2342cdc68cc0165
Celiaplt <celia19.pilot01@gmail.com>
2024-06-12 16:42:55 +0200

    Premier commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
 import './index.css'
 
-// import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
-// register Swiper custom elements
 register();
 
 import "@fontsource/outfit";
 import "@fontsource/roboto";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
