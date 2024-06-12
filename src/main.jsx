322cf78958fd9a607a06a4e90e29f30f36d0dfe2
Celiaplt <celia19.pilot01@gmail.com>
2024-06-12 17:00:56 +0200

    Cinquième commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-import { register } from 'swiper/element/bundle';
-register();
+// import { register } from 'swiper/element/bundle';
+// register();
 
-import "@fontsource/outfit";
-import "@fontsource/roboto";
+// import "@fontsource/outfit";
+// import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
